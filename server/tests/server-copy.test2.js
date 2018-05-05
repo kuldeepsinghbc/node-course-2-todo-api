f6bd4565815e9edf6396b6fc7717b02c41c4e97d
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-05-05 09:10:12 -0700

    Added tests for GET /todos

--- server/tests/server-copy.test2.js
+++ server/tests/server-copy.test2.js
@@ -16,14 +16,13 @@ const todos = [{
 
 beforeEach((done) => {
   Todo.remove({}).then(() => {
-    Todo.insertMany(todos);
+    return Todo.insertMany(todos);
   }).then(() => done());
 });
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
     var text = 'Test todo text';
-
     request(app)
       .post('/todos')
       .send({
@@ -37,7 +36,6 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         }
-
         Todo.find({
           text
         }).then((todos) => {
@@ -47,8 +45,7 @@ describe('POST /todos', () => {
         }).catch((e) => done(e));
       });
   });
-
-  it('should not create todo with invalid body data', (done) => {
+  it('should not create todo with invalid data', (done) => {
     request(app)
       .post('/todos')
       .send({})
@@ -57,23 +54,10 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         }
-
         Todo.find().then((todos) => {
           expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
-  });
-});
-
-describe('GET /todos', () => {
-  it('should get all todos', (done) => {
-    request(app)
-      .get('/todos')
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.todos.length).toBe(2);
-      })
-      .end(done);
-  });
+  })
 });
