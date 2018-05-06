560bfeb820d705186831dac9a764e543d6120a5b
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-05-06 11:42:03 -0700

    Added GET/todo with id

--- server/server.js
+++ server/server.js
@@ -1,41 +1,67 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
+var { ObjectID } = require("mongodb");
 
-
-var {
-  mongoose
-} = require('./db/mongoose');
-var {
-  Todo
-} = require('./models/todo');
-var {
-  User
-} = require('./models/user');
+var { mongoose } = require("./db/mongoose");
+var { Todo } = require("./models/todo");
+var { User } = require("./models/user");
 
 var app = express();
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post("/todos", (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  })
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({
-      todos
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({
+        todos
+      });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+// GET /todos by id
+// validate id using isValid
+// 404 -send back empty send
+
+// findById
+// success
+// if todo - send it back
+// if no todo - send back with empty body
+// error
+// 400- and send empty body back
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
     });
-  }, (e) => {
-    res.status(400).send(e);
-  });
 });
 app.listen(3000, () => {
-  console.log('Started on port 3000');
+  console.log("Started on port 3000");
 });
 module.exports = {
   app
