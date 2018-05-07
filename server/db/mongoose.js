ed4b9911a151d3913a9208bf07b5b608e1a2c72f
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-05-07 16:21:51 -0700

    Setup separate test and development evns

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,7 +7,7 @@ mongoose.Promise = global.Promise;
 // mongoose.connect(db.localhost || db.mlab);
 // mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGO_URI);
 module.exports = {
   mongoose
 };
